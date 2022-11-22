e1e14e9b33837576b77eb8f6486f6e1fe71e6b3d
hakochan2 <hakozakiyuhi@gmail.com>
2022-11-22 12:05:37 -0500

    Google updated checkbox html

--- server/src/main/java/com/example/demo/credentials/Google.java
+++ server/src/main/java/com/example/demo/credentials/Google.java
@@ -59,6 +59,8 @@ public class Google {
 		driver.findElement(By.id("i4")).click();
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 		driver.findElement(By.id("i5")).click();
+
+		//Final Step
         //driver.findElement(By.xpath("//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/div[2]/c-wiz/div/div[3]/form/div/div[3]/div/div/button")).click();
 	}
 }
